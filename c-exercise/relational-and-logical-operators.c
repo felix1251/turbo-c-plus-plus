a953f6dd4d4c8e982ef3e4ecce47d546ccc542ad
felix1251 <domhack123@gmail.com>
2023-07-03 11:33:26 +0800

    ..

--- c-exercise/relational-and-logical-operators.c
+++ c-exercise/relational-and-logical-operators.c
@@ -21,12 +21,13 @@ int main(void)
 
    // 1 means true and 0 means false
 
-   printf("expression is: %d\n", !(1 && 0));
+   printf("expression is: %d and %d\n", !(1 && 0), (1 && 0));
 
    int x;
 
    x = 100;
-   printf("check if x is greater than 10: %d", x > 10);
+   printf("check if x is greater than 10: %d\n", x > 10);
+   printf("check if x is lesser than 10: %d", x < 10);
 
    return 0;
 }
