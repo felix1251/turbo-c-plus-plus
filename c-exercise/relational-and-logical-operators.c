bb11b578d0c56627fef6dd08936257f52907f8fd
felix1251 <domhack123@gmail.com>
2023-07-03 11:22:34 +0800

    relational and logical operators

---
+++ c-exercise/relational-and-logical-operators.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+        -----Relational Operations-----
+        Operator     Action
+        >            Greater than
+        >=           Greater than or equal
+        <            Less than
+        <=           Less than or equal
+        ==           Equa
+        !=           Not equal
+
+        -----Logical Operations-----
+        Operator     Action
+        &&           AND
+        ||           OR
+        !            NOT
+    */
+
+   int x;
+
+   x = 100;
+   printf("%d ", x > 10);  // 1 means true and 0 means false
+
+   return 0;
+}
